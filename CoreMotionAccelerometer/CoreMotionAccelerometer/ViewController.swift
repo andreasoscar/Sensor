defb2e056cb38c364dfed16cb4de4a39a7b84014
016172 <andreas@Andreass-MacBook-Pro.local>
2020-05-31 14:09:14 +0200

    acc.

---
+++ CoreMotionAccelerometer/CoreMotionAccelerometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMotionAccelerometer
+//
+//  Created by Andreas Oscar Olsson on 2020-05-31.
+//  Copyright © 2020 Andreas Oscar Olsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
